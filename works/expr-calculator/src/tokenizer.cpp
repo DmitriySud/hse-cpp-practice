8ae791b1facd552d7808073eb3589dfb199fbe31
dimasya <dyusudakov@edu.hse.ru>
2021-10-25 04:23:09 +0300

    expr-calculator

---
+++ works/expr-calculator/src/tokenizer.cpp
@@ -0,0 +1,82 @@
+#include "tokenizer.hpp"
+
+namespace calc::tokenizer {
+
+namespace {
+
+int ToDigit(char c) {
+    return c - '0';
+}
+
+}  // namespace
+
+Tokenizer::Tokenizer(std::unique_ptr<std::istream> input) : input_(std::move(input)) {
+    Next();
+}
+
+Tokenizer::Tokenizer(Tokenizer&& other)
+    : input_(std::move(other.input_)), is_end_(other.is_end_), cur_token_(other.cur_token_) {
+}
+
+//Tokenizer::Tokenizer(const Tokenizer& other)
+    //: input_(other.input_), is_end_(other.is_end_), cur_token_(other.cur_token_) {
+    //Next();
+//}
+
+Tokenizer& Tokenizer::operator=(Tokenizer&& other) {
+  input_ = std::move(other.input_);
+  cur_token_ = other.cur_token_;
+  is_end_ = other.is_end_;
+  
+  return *this;
+}
+
+void Tokenizer::SkipEmpty() {
+    while (!input_->eof() && input_->peek() == ' ') {
+        input_->get();
+    }
+}
+
+bool Tokenizer::IsEnd() {
+    return is_end_;
+}
+
+void Tokenizer::Next() {
+  SkipEmpty();
+  is_end_ = input_->eof();
+
+  if (is_end_) {
+    return;
+  }
+
+  char cur_c = input_->get();
+  if (cur_c == '(') {
+      cur_token_ = BracketToken::kOpen;
+  } else if (cur_c == ')') {
+      cur_token_ = BracketToken::kClose;
+  } else if (cur_c == '*') {
+      cur_token_ = MulOpToken::kMult;
+  } else if (cur_c == '/') {
+      cur_token_ = MulOpToken::kDiv;
+  } else if (cur_c == '%') {
+      cur_token_ = MulOpToken::kModule;
+  } else if (cur_c == '-') {
+      cur_token_ = AddOpToken::kMinus;
+  } else if (cur_c == '+') {
+      cur_token_ = AddOpToken::kPlus;
+  } else if (std::isdigit(cur_c)) {
+      std::string res_str;
+      res_str.push_back(cur_c);
+      while (!input_->eof() && std::isdigit(input_->peek())) {
+          cur_c = input_->get();
+          res_str.push_back(cur_c);
+      }
+      cur_token_ = NumberToken{big_numbers::BigInteger(res_str)};
+  }
+}
+
+Token Tokenizer::GetToken() {
+  return cur_token_;
+}
+
+}
---
+++ works/expr-calculator/src/tokenizer.hpp
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <istream>
+#include <variant>
+#include <memory>
+
+#include <big_integer.hpp>
+
+namespace calc::tokenizer {
+
+struct NumberToken {
+  big_numbers::BigInteger value;
+};
+
+enum class BracketToken {
+  kOpen, kClose
+};
+
+enum class AddOpToken {
+  kPlus, kMinus
+};
+
+enum class MulOpToken {
+  kMult, kDiv, kModule
+};
+
+using Token =
+    std::variant<NumberToken, BracketToken, AddOpToken, MulOpToken>;
+
+class Tokenizer {
+public:
+  Tokenizer() = delete;
+  Tokenizer(std::unique_ptr<std::istream>);
+
+  Tokenizer(Tokenizer&&);
+  Tokenizer(const Tokenizer&) = delete;
+
+  Tokenizer& operator=(Tokenizer&& other);
+  Tokenizer& operator=(const Tokenizer&) = delete;
+  
+
+  bool IsEnd();
+
+  void Next();
+
+  Token GetToken();  
+
+private:
+  void SkipEmpty();
+
+  std::unique_ptr<std::istream> input_;
+  bool is_end_{false};
+  Token cur_token_{};
+};
+
+}
